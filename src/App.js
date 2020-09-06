52aa5e215b0a77cbebe2cfbf98b9dfddefeb6f17
Sourab Hossain <sourabhossain.fci@gmail.com>
2020-09-07 01:47:30 +0600

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div>
-       
+       <h1>Hello World!</h1>
     </div>
   );
 }
