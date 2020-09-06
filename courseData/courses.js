6f4db3b0f851b666c8450882124a7c6f729be99e
Sourab Hossain <sourabhossain.fci@gmail.com>
2020-09-07 03:47:23 +0600

    add data

---
+++ courseData/courses.js
@@ -0,0 +1,139 @@
+const courses = [
+    {
+        id: 1,
+        name: 'The Web Developer Bootcamp',
+        description: 'The only course you need to learn web development - HTML, CSS, JS, Node, and More!',
+        mentor: 'Colt Steele',
+        img: 'https://img-a.udemycdn.com/course/240x135/625204_436a_2.jpg',
+        rating: 4.7,
+        price: 129.99
+    },
+    {
+        id: 2,
+        name: '2020 Complete Python Bootcamp: From Zero to Hero in Python',
+        description: 'Learn Python like a Professional! Start from the basics and go all the way to creating your own applications and games!',
+        mentor: 'Jose Portilla',
+        img: 'https://img-a.udemycdn.com/course/240x135/567828_67d0.jpg',
+        rating: 4.6,
+        price: 131.99
+    },
+    {
+        id: 3,
+        name: 'The Complete Communication Skills Master Class for Life',
+        description: 'Communication Skills for Persuasion, Assertiveness and all Business Communication Needs',
+        mentor: 'TJ Walker',
+        img: 'https://img-a.udemycdn.com/course/240x135/1802404_48e9_2.jpg',
+        rating: 4.3,
+        price: 129.99
+    },
+    {
+        id: 4,
+        name: 'Modern React with Redux [2020 Update]',
+        description: 'Master React v16.6.3 and Redux with React Router, Webpack, and Create-React-App. Includes Hooks!',
+        mentor: 'Stephen Robert',
+        img: 'https://img-a.udemycdn.com/course/240x135/705264_caa9_11.jpg',
+        rating: 4.7,
+        price: 139.99
+    },
+    {
+        id: 5,
+        name: 'The Ultimate Hands-On Hadoop - Tame your Big Data!',
+        description: 'Hadoop tutorial with MapReduce, HDFS, Spark, Flink, Hive, HBase, MongoDB, Cassandra, Kafka + more! Over 25 technologies.',
+        mentor: 'Frank Kane',
+        img: 'https://img-a.udemycdn.com/course/240x135/996228_af5f_3.jpg',
+        rating: 4.5,
+        price: 129.99
+    },
+    {
+        id: 6,
+        name: 'The Data Science Course 2020: Complete Data Science Bootcamp',
+        description: 'Complete Data Science Training: Mathematics, Statistics, Python, Advanced Statistics in Python, Machine & Deep Learning',
+        mentor: '365 Careers Team',
+        img: 'https://img-a.udemycdn.com/course/240x135/1754098_e0df_3.jpg',
+        rating: 4.5,
+        price: 129.99
+    },
+    {
+        id: 7,
+        name: 'The Complete Financial Analyst Course 2020',
+        description: 'Excel, Accounting, Financial Statement Analysis, Business Analysis, Financial Math, PowerPoint: Everything is Included!',
+        mentor: '365 Careers Team',
+        img: 'https://img-a.udemycdn.com/course/240x135/648826_f0e5_4.jpg',
+        rating: 4.5,
+        price: 129.99
+    },
+    {
+        id: 8,
+        name: 'The Ultimate Drawing Course - Beginner to Advanced',
+        description: 'Learn the #1 most important building block of all art',
+        mentor: 'Jaysen Batchelor',
+        img: 'https://img-a.udemycdn.com/course/240x135/874012_c7f2_3.jpg',
+        rating: 4.5,
+        price: 129.99
+    },
+    {
+        id: 9,
+        name: 'Learn Python Programming Masterclass',
+        description: 'This Python For Beginners Course Teaches You The Python Language Fast. Includes Python Online Training With Python 3',
+        mentor: 'Jean-Paul Roberts',
+        img: 'https://img-a.udemycdn.com/course/240x135/629302_8a2d_2.jpg',
+        rating: 4.5,
+        price: 129.99
+    },
+    {
+        id: 10,
+        name: 'Angular - The Complete Guide (2020 Edition)',
+        description: 'Master Angular 10 (formerly "Angular 2") and build awesome, reactive web apps with the successor of Angular.js ',
+        mentor: 'Maximilian Schwarzmüller',
+        img: 'https://img-a.udemycdn.com/course/240x135/756150_c033_2.jpg',
+        rating: 4.6,
+        price: 129.99
+    },
+    {
+        id: 11,
+        name: 'The Complete Digital Marketing Course - 12 Courses in 1 ',
+        desciption: 'Master Digital Marketing Strategy, Social Media Marketing, SEO, YouTube, Email, Facebook Marketing, Analytics & More!',
+        mentor: 'Rob Percival',
+        img: 'https://img-a.udemycdn.com/course/240x135/914296_3670_8.jpg',
+        rating: 4.4,
+        price: 129.99
+    },
+    {
+        id: 12,
+        name: 'The Complete Node.js Developer Course (3rd Edition)',
+        desciption: 'Learn Node.js by building real-world applications with Node, Express, MongoDB, Jest, and more!',
+        mentor: 'Andrew Mead',
+        img: 'https://img-a.udemycdn.com/course/240x135/922484_52a1_8.jpg',
+        rating: 4.7,
+        price: 84.99
+    },
+    {
+        id: 13,
+        name: 'Learn Ethical Hacking From Scratch',
+        desciption: 'Become an ethical hacker that can hack computer systems like black hat hackers and secure them like security experts.',
+        mentor: 'Zaid Sabih',
+        img: 'https://img-a.udemycdn.com/course/240x135/857010_8239_2.jpg',
+        rating: 4.5,
+        price: 129.99
+    },
+    {
+        id: 14,
+        name: 'Beginning C++ Programming - From Beginner to Beyond',
+        desciption: 'Obtain Modern C++ Object-Oriented Programming (OOP) and STL skills needed for game, system, and application development. ',
+        mentor: 'Frank J. Mitropoulos',
+        img: 'https://img-a.udemycdn.com/course/240x135/1576854_9aeb.jpg',
+        rating: 4.6,
+        price: 129.99
+    },
+    {
+        id: 15,
+        name: 'Complete C# Unity Game Developer 2D',
+        desciption: 'Learn Unity in C# & Code Your First Seven 2D Video Games for Web, Mac & PC. The Tutorials Cover Tilemap (35 hours) ',
+        mentor: 'Ben Tristem',
+        img: 'https://img-a.udemycdn.com/course/240x135/258316_55e9_8.jpg',
+        rating: 4.6,
+        price: 129.99
+    },
+]
+
+export default courses;
